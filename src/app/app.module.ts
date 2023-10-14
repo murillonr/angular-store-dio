c0c9900aefff73605afbe200e2d228f1a66008ac
Murillo Negreiros <murillonr89@gmail.com>
2023-10-14 20:00:37 -0300

    correções depois de problemas de importações de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './componentes/card/card.component';
 import { MenuBarComponent } from './componentes/menu-bar/menu-bar.component';
+import { CardLabelComponent } from './componentes/card/card-label/card-label.component';
+import { CardPricingComponent } from './componentes/card/card-pricing/card-pricing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CardComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    CardLabelComponent,
+    CardPricingComponent
   ],
   imports: [
     BrowserModule,
